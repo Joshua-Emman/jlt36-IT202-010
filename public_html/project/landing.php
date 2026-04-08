cc600030bbc299c97c262a429a8c925fd867d204
josh <joshua.tenecio@gmail.com>
2026-04-08 13:12:39 -0400

    fixed must be logged in message

--- public_html/project/landing.php
+++ public_html/project/landing.php
@@ -1,11 +1,12 @@
 <?php
-require(__DIR__ . "/../../partials/nav.php");
-
+require(__DIR__ . "/../../lib/functions.php");
 if (!is_logged_in()) {
     flash("You must be logged in to view this page.", "danger");
     header("Location: login.php");
     exit;
 }
+require(__DIR__ . "/../../partials/nav.php");
+
 error_log("Session: " . var_export($_SESSION, true));
 ?>
 <h1>Landing Page</h1>
