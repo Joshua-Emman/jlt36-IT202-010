385005922f4896da571cf737f1dce28088a883d4
josh <joshua.tenecio@gmail.com>
2026-04-08 13:54:35 -0400

    I ACTUALLY FIXED THE MUST LOG IN MESSAGE THIS TIME

--- public_html/project/landing.php
+++ public_html/project/landing.php
@@ -1,8 +1,11 @@
 <?php
 require(__DIR__ . "/../../lib/functions.php");
+
+session_start();
+
 if (!is_logged_in()) {
     flash("You must be logged in to view this page.", "danger");
-    header("Location: $BASE_PATH/login.php");
+    header("Location: /project/login.php");
     exit;
 }
 require(__DIR__ . "/../../partials/nav.php");
