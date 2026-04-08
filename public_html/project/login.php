7b41c1e0cece716556d948c044f52050663442f8
josh <joshua.tenecio@gmail.com>
2026-04-08 17:16:43 -0400

    trying to fix redirect to landing

--- public_html/project/login.php
+++ public_html/project/login.php
@@ -1,5 +1,5 @@
 <?php
-require(__DIR__ . "/../../partials/nav.php");
+
 ?>
 <h3>Login</h3>
 <form onsubmit="return validate(this)" method="POST">
@@ -128,7 +128,7 @@ if (isset($_POST["email"], $_POST["password"])) {
                         $_SESSION["user"]["roles"] = isset($roles) ? $roles : [];
                         
                         error_log("Login Session Data: " . var_export($_SESSION["user"], true));
-                        header("Location: landing.php");
+                        header("Location: /project/landing.php");
                     } else {
                         //echo "Invalid password<br>";
                         $ambigify = true; // ambiguous login attempt
